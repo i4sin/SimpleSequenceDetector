18120e6776234225079ded0822dd5e9691aee7fc
Yasin Mooasvi <smym4244@gmail.com>
2024-10-29 20:53:35 +0330

    created the initial state machine for pattern detector

--- RTL/SimpleSequenceDetector/src/SimpleSequenceDetector.sv
+++ RTL/SimpleSequenceDetector/src/SimpleSequenceDetector.sv
@@ -1,36 +1,40 @@
 module SimpleSequenceDetector(
     input clk,
     input resetn,
-    input [31:0] A,
-    input [31:0] B,
-    input [2:0] OP,
-    output [31:0] X,
-    output Z
-);
-    logic [31:0] output_d, output_q;
 
-    assign Z = (X == 0);
-    assign X = output_q;
+    input seq,
+    input valid,
+    output detected
+);
+    typedef enum {
+        StIdle,
+        St1,
+        St10,
+        St101,
+        St1011,
+        St10110
+    } stete_e;
+
+    state_e state_d, state_q;
 
     always_ff @(posedge clk or negedge resetn) begin
         if (!resetn) begin
-            output_q <= 0;
+            state_q <= StIdle;
         end else begin
-            output_q <= output_d;
+            state_q <= state_d;
         end
     end
 
     always_comb begin
-        output_d = output_q;
-        case (OP)
-            3'b000 : output_d = A + B;
-            3'b001 : output_d = A - B;
-            3'b010 : output_d = ~(A | B);
-            3'b011 : output_d = A | B;
-            3'b100 : output_d = ~(A & B);
-            3'b101 : output_d = A & B;
-            3'b110 : output_d = A ~^ B;
-            default: output_d = 0;
+        state_d = state_q;
+        detected = 0;
+        case (state_q)
+            StIdle  : state_d = (seq ? St1    : StIdle );
+            St1     : state_d = (seq ? St1    : St10   );
+            St10    : state_d = (seq ? St101  : St10   );
+            St101   : state_d = (seq ? St1011 : St101  );
+            St1011  : state_d = (seq ? St1011 : St10110);
+            default : state_d = StIdle;
         endcase
     end
 endmodule
