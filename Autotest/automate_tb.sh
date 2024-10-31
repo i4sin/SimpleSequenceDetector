6255141feda2ef135e3f2844b75383eeac38512d
Yasin Mooasvi <smym4244@gmail.com>
2024-10-31 09:09:38 +0330

    changed automate_tb script to avoid junk comments

--- Autotest/automate_tb.sh
+++ Autotest/automate_tb.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -eu
-# DIR=$(dirname "${BASH_SOURCE[0]}")
-# REPO=$(readlink -f $DIR/../../..)
-# source $REPO/Utils/utility-scripts.sh
-# set_prompt
-
-# QUESTA_DOCKER_AUTHORIZED_KEYS_FILE=$REPO/AutoTest/keys/fabric_key.pub
 
 main() {
     trap remove_docker EXIT
@@ -19,12 +13,8 @@ main() {
         -v /home/yasin/projects/RTL/:/workspace \
         questasim-10.7c-automation:0.1.0)
 
-    # AUTHORIZED_KEYS=$(cat $QUESTA_DOCKER_AUTHORIZED_KEYS_FILE)
-    # docker exec $CONTAINER_ID /bin/bash -c "echo $AUTHORIZED_KEYS >> /root/.ssh/authorized_keys"
     docker exec $CONTAINER_ID /bin/bash -c "echo export DISPLAY=$DISPLAY >> /root/.profile"
-
     docker exec -it $CONTAINER_ID /bin/bash || true
-    # docker exec -it $CONTAINER_ID /bin/bash -c 'vsim -c -do "vlib work; vlog ALU.sv ALU_tb.sv; vsim work.ALU_tb; run -all; quit"' || true
 }
 
 remove_docker() {
