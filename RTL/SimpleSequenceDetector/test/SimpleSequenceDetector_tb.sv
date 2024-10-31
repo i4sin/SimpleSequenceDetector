e8bc615af1269ba2742c76bffc0dcc3e3a3586ed
Yasin Mooasvi <smym4244@gmail.com>
2024-10-31 14:25:35 +0330

    fixed SimpleSequenceDetector tb

--- RTL/SimpleSequenceDetector/test/SimpleSequenceDetector_tb.sv
+++ RTL/SimpleSequenceDetector/test/SimpleSequenceDetector_tb.sv
@@ -5,7 +5,7 @@ import vunit_pkg::*;
 `include "vunit_defines.svh"
 
 module SimpleSequenceDetector_tb();
-    localparam TOTAL_WORDS_COUNT = 100;
+    localparam TOTAL_WORDS_COUNT = 100000;
 
     typedef logic logic_array[$];
     typedef enum {
@@ -36,6 +36,18 @@ module SimpleSequenceDetector_tb();
         .detected(output_detected)
     );
 
+    task drive_base_pattern();
+        drive_input(1);
+        drive_input(0);
+        drive_overlapping_pattern();
+    endtask
+
+    task drive_overlapping_pattern();
+        drive_input(1);
+        drive_input(1);
+        drive_input(0);
+    endtask
+
     function logic_array generate_random_seq();
         logic random_seed;
         logic_array random_seq;
@@ -47,7 +59,7 @@ module SimpleSequenceDetector_tb();
                 2: random_seq = append_overlapping_pattern(random_seq);
             endcase
         end
-        $display("random_seq: %p", random_seq);
+        $display("random_seq:\n %p", random_seq);
         return random_seq;
     endfunction
 
@@ -75,49 +87,48 @@ module SimpleSequenceDetector_tb();
     endfunction
 
     function automatic logic_array expect_detected_output(logic_array seq_array);
-        logic current_seq_item;
-        logic current_detected;
-
         state current_state = StIdle;
         state next_state = StIdle;
-        
+
+        logic current_detected;
+
         logic_array expected_detected_array;
         
-        while (seq_array.size()) begin
-            current_seq_item = seq_array.pop_front();
+        foreach (seq_array[i]) begin
             case (current_state)
                 StIdle : begin
                     current_detected = 0;
-                    next_state = (current_seq_item ? St1 : StIdle);
+                    next_state = (seq_array[i] ? St1 : StIdle);
                 end
                 St1 : begin
                     current_detected = 0;
-                    next_state = (current_seq_item ? St1 : St10);
+                    next_state = (seq_array[i] ? St1 : St10);
                 end
                 St10 : begin
                     current_detected = 0;
-                    next_state = (current_seq_item ? St101 : StIdle);
+                    next_state = (seq_array[i] ? St101 : StIdle);
                 end
                 St101 : begin
                     current_detected = 0;
-                    next_state = (current_seq_item ? St1011 : St10);
+                    next_state = (seq_array[i] ? St1011 : St10);
                 end
                 St1011 : begin
-                    current_detected = !current_seq_item;
-                    next_state = (current_seq_item ? St1 : St10110);
+                    current_detected = !seq_array[i];
+                    next_state = (seq_array[i] ? St1 : St10110);
                 end
                 St10110 : begin
                     current_detected = 0;
-                    next_state = (current_seq_item ? St101 : StIdle);
+                    next_state = (seq_array[i] ? St101 : StIdle);
                 end
                 default : begin
                     current_detected = 0;
                     next_state = StIdle;
                 end
             endcase
+            current_state = next_state;
             expected_detected_array.push_back(current_detected);
         end
-        $display("expected_detected_array: %p", expected_detected_array);
+        $display("expected_detected_array:\n %p", expected_detected_array);
         return expected_detected_array;
     endfunction
 
@@ -128,7 +139,6 @@ module SimpleSequenceDetector_tb();
     endtask
 
     task check_output(logic current_expected_output);        
-        @(posedge clk);
         assert (output_detected == current_expected_output) begin
             $display("seq: %h, detected: %h, expected_output: %h",
                         input_seq, output_detected, current_expected_output);
@@ -144,6 +154,21 @@ module SimpleSequenceDetector_tb();
             repeat(6) @(posedge clk);
         end
         
+        `TEST_CASE("direct_base_test") begin
+            drive_base_pattern();
+            check_output(1);
+        end
+
+        `TEST_CASE("direct_overlapping_test") begin
+            automatic int overlapping_tests_count = $urandom_range(1, 10);
+            drive_base_pattern();
+            check_output(1);
+            for (int i = 0; i < overlapping_tests_count; i++) begin
+                drive_overlapping_pattern();
+                check_output(1);
+            end
+        end
+
         `TEST_CASE("random_test_with_back_pressure") begin
             seq_array = generate_random_seq();
             expected_detected_array = expect_detected_output(seq_array);
@@ -156,7 +181,6 @@ module SimpleSequenceDetector_tb();
 
         `TEST_CASE_CLEANUP begin
             $display("Making Cleanup....");
-            assert(0);
         end
 
         // `WATCHDOG(10000ns)
