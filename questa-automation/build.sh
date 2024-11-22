3be23a7a8293c2532a95134f8825e6405d4b4a47
Yasin Moosavi <smym4244@gmail.com>
2024-11-22 20:50:32 +0330

    Delete questa-automation directory

--- questa-automation/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-docker build --network host \
-    -t questasim-10.7c-automation:0.1.0 \
-    .
-
-# `--network host` is used for proxy. Read following link for more information:
-# https://stackoverflow.com/questions/61590317/use-socks5-proxy-from-host-for-docker-build
